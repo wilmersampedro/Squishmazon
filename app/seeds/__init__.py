93927c01de6e79152b701572da69a6acaaee47c0
Wilmer <wilmer.sampedro@gmail.com>
2024-02-04 14:09:01 -0800

    work

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -18,19 +18,19 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
-    if environment == 'production':
-        # Before seeding in production, you want to run the seed undo
-        # command, which will  truncate all tables prefixed with
-        # the schema name (see comment in users.py undo_users function).
-        # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_products()
-        undo_reviews()
-        undo_product_images()
-        undo_categories()
-        undo_cart_items()
-        undo_orders()
-        undo_order_items()
+    # if environment == 'production':
+    #     # Before seeding in production, you want to run the seed undo
+    #     # command, which will  truncate all tables prefixed with
+    #     # the schema name (see comment in users.py undo_users function).
+    #     # Make sure to add all your other model's undo functions below
+    #     undo_users()
+    #     undo_products()
+    #     undo_reviews()
+    #     undo_product_images()
+    #     undo_categories()
+    #     undo_cart_items()
+    #     undo_orders()
+    #     undo_order_items()
     # Add other seed functions here
     seed_users()
     seed_products()
