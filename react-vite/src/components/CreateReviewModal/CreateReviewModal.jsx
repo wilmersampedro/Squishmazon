a5e257135b81e75f4db93e5d22640bc262853732
Wilmer <wilmer.sampedro@gmail.com>
2024-02-06 11:56:25 -0800

    added styling and error handling for creating review

--- react-vite/src/components/CreateReviewModal/CreateReviewModal.jsx
+++ react-vite/src/components/CreateReviewModal/CreateReviewModal.jsx
@@ -16,7 +16,9 @@ function CreateReviewModal({ product }) {
     e.preventDefault();
     const errs = {};
     if (reviewText.length > 350) errs.reviewText = "Review text must be less than 350 characters"
+    if(!reviewText.length) errs.reviewText = "Please include a written review"
     if (stars < 1 || stars > 5) errs.stars = "Star rating must be between 1-5"
+    if (!stars) errs.stars = "Please select a star rating"
     if (Object.keys(errs).length) return setErrors(errs)
 
     const body = {
@@ -34,20 +36,9 @@ function CreateReviewModal({ product }) {
 
   return (
     <>
-      <div id="editModalTitle">Post your review</div>
+      <div id="editModalTitle">Create Review</div>
       <form onSubmit={handleSubmit} >
-        <div>
-          <label htmlFor="reviewText">
-            Review
-          </label>
-          <textarea
-            name="reviewText"
-            value={reviewText}
-            placeholder="Leave your review here..."
-            onChange={(e) => setReviewText(e.target.value)}
-          />
-        </div>
-
+        <div id="overallRating">Overall Rating</div>
         <div id="stars-container" onMouseLeave={() => { setActiveRating(stars) }}>
           <div value={1} onClick={() => onChange(1)} onMouseEnter={() => setActiveRating(1)} className={activeRating >= 1 ? 'filled' : 'empty'}>
             <i className="fa-solid fa-star"></i>
@@ -65,8 +56,31 @@ function CreateReviewModal({ product }) {
             <i className="fa-solid fa-star"></i>
           </div>
         </div>
-        <div>
-          <div onClick={closeModal}>Cancel</div>
+        <div className="form-errors">
+          {errors.stars}
+        </div>
+        
+        <div id="reviewTextContainer">
+          <label htmlFor="reviewText">
+            Add a written review
+          </label>
+
+          <textarea
+          id="reviewTxt"
+            name="reviewText"
+            value={reviewText}
+            placeholder="What did you like or dislike? Leave your review here..."
+            rows="7"
+            cols="50"
+            onChange={(e) => setReviewText(e.target.value)}
+          />
+          <div className="form-errors">
+          {errors.reviewText}
+        </div>
+        </div>
+        <br />
+        <div id="submitModalBtns">
+          <div id="cancelBtn" onClick={closeModal}>Cancel</div>
           <input type="submit" />
         </div>
       </form>
--- react-vite/src/components/ProductDetail/ProductDetail.jsx
+++ react-vite/src/components/ProductDetail/ProductDetail.jsx
@@ -18,6 +18,32 @@ function ProductDetail() {
   const reviews = useSelector((state) => state.review)
   const user = useSelector((state) => state.session.user)
 
+  const months = {
+    Jan: "January",
+    Feb: "February",
+    Mar: "March",
+    Apr: "April",
+    May: "May",
+    Jun: "June",
+    Jul: "July",
+    Aug: "August",
+    Sep: "September",
+    Oct: "October",
+    Nov: "November",
+    Dec: "December"
+  }
+  const dateFormatter = (date) => {
+    const splitDate = date.split(" ")
+    let day;
+    if (splitDate[1][0] == 0) {
+      day = splitDate[1][1]
+    } else {
+      day = splitDate[1]
+    }
+    const month = splitDate[2]
+    return  `${months[month]}, ${day} ${splitDate[3]}`
+  }
+
   useEffect(() => {
     dispatch(thunkFetchReviews(productId))
   }, [dispatch, productId])
@@ -50,17 +76,26 @@ function ProductDetail() {
         </div>
 
       </div>
-      <br />
+      {/* <br /> */}
       <section id="reviewsSection">
         <div id="reviewSectionLeft">
+            <div id="reviewSectionLeftHeader">Customer reviews</div>
           <div id="reviewSectionStars">
             <div className={product.avg_reviews == 5 ? "star-5 revSec5" : product.avg_reviews == 4 ? "star-4 revSec4" : product.avg_reviews == 3 ? "star-3 revSec3" : product.avg_reviews == 2 ? "star-2 revSec2" : product.avg_reviews == 1 ? "star-1 revSec1" : "star-0 revSec0"}><span className="revSecAvg">{product.avg_reviews} out of 5</span> </div>
           </div>
+          <div id="globalRatings">{product?.num_reviews} global {product?.num_reviews == 1 ? "rating" : "ratings"}</div>
+          <br />
           <div>
-            {(user && product?.vendor_id !== user.id && existingReviewCheck(user.id) === false) && <OpenModalButton
-              buttonText="Post Your Review"
+            {(user && product?.vendor_id !== user.id && existingReviewCheck(user.id) === false) &&
+            <>
+            <div id="revPromptLeft">Review this product</div>
+            <div id="revPromptL2">Share your thoughts with other customers</div>
+            <OpenModalButton
+              id="postRevBtn"
+              buttonText="Write a customer review"
               modalComponent={<CreateReviewModal product={product} />}
-            />}
+              />
+              </>}
           </div>
         </div>
         <div >
@@ -69,26 +104,32 @@ function ProductDetail() {
 
             return reviewsToDisplay.length ?
               <div id="productReviewsSection"><span id="customersSay">Customers say</span>
-                {reviewsToDisplay.map(r => <div
+                {reviewsToDisplay.map(r =>
+                <div
                   key={r.id}
                 >
-
+                  <br />
                   <div>{r?.author["first_name"]} {r?.author["last_name"]}</div>
-                  <div>{r?.stars} stars</div>
-                  <div>Reviewed on: {r?.created_at}</div>
+                  <div className={product.avg_reviews == 5 ? "star-5" : product.avg_reviews == 4 ? "star-4" : product.avg_reviews == 3 ? "star-3" : product.avg_reviews == 2 ? "star-2" : product.avg_reviews == 1 ? "star-1" : "star-0"}></div>
+
+                  <div>Reviewed on: {dateFormatter(r?.created_at)}</div>
                   <div>{r?.review_text}</div>
+                  <div id="revBtnContainer">
                   {r?.user_id == user?.id && <OpenModalButton
+                    id="editRevBtn"
                     buttonText="Edit"
                     modalComponent={<EditReviewModal review={r} />}
                     onClick={(e) => {
                       e.stopPropagation()
                     }}
 
-                  />}
+                    />}
                   {r?.user_id == user?.id && <OpenModalButton
+                    id="deleteRevBtn"
                     buttonText="Delete"
                     modalComponent={<DeleteReviewModal review={r} />}
-                  />}
+                    />}
+                    </div>
                 </div>)}
               </div> :
               <div>
