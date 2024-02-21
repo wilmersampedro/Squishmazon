5b5eb75d75d08b00fe810f65683f9d672fc4be12
Wilmer <wilmer.sampedro@gmail.com>
2024-02-20 18:15:51 -0800

    trying to do something different w/ button

--- react-vite/src/components/ProductDetail/ProductDetail.jsx
+++ react-vite/src/components/ProductDetail/ProductDetail.jsx
@@ -85,9 +85,10 @@ function ProductDetail() {
     productInWishlist(user.wishlist)
   },[dispatch, user.wishlist, productInWishlist])
 
-  const handleWishlistAdd = (e) => {
-    dispatch(thunkAddWishlistItem(product))
-    setWishlistButton(false)
+  const handleWishlistAdd = async (e) => {
+    const wishlistStatus = await dispatch(thunkAddWishlistItem(product))
+    console.log("🚀 ~ handleWishlistAdd ~ wishlistStatus:", wishlistStatus)
+    // setWishlistButton(false)
   }
 
 
@@ -106,7 +107,16 @@ function ProductDetail() {
           <div className={product.avg_reviews == 5 || product.avg_reviews >= 4.8 ? "star-5" : product.avg_reviews < 4.8 && product.avg_reviews >= 4.3 ? "star-4-5" : product.avg_reviews < 4.3 && product.avg_reviews >= 3.8 ? "star-4" : product.avg_reviews < 3.8 && product.avg_reviews >= 3.3 ? "star-3-5" : product.avg_reviews < 3.3 && product.avg_reviews >= 2.8 ? "star-3" : product.avg_reviews < 2.8 && product.avg_reviews >= 2.3 ? "star-2-5" : product.avg_reviews < 2.3 && product.avg_reviews >= 1.8 ? "star-2" : product.avg_reviews < 1.8 && product.avg_reviews >= 1.3 ? "star-1-5" : product.avg_reviews < 1.3 && product.avg_reviews >= .8 ? "star-1" : product.avg_reviews < .8 && product.avg_reviews >= .3 ? "star-half" : "star-0"}><span className="numReviews">{product.num_reviews} {product.num_reviews == 1 ? "Review" : "Reviews"}</span> </div>
           </div>
           <div id="productDetailsPrice"><span id="productDetailPriceSpan">Price:</span> ${product?.price.toFixed(2)}</div>
-        {(user.id != product.vendor_id && !productInWishlist(user.wishlist)) && <button onClick={handleWishlistAdd}>Add to Wish List</button>}
+        {user.id != product.vendor_id && <OpenModalButton
+              id="wishlistBtn"
+              buttonText="Add to Wish List"
+              onButtonClick={handleWishlistAdd}
+              modalComponent={<>
+                <div>
+                  {productInWishlist(product) ? "" : ""}
+                </div>
+              </>}
+              />}
           <div id="productDetailsInStock">{product?.in_stock && <i class="fa-solid fa-check" style={{ "color": "#33A3FF" }}></i>} {product?.in_stock ? 'In Stock!' : 'Out of Stock'}</div>
           <div id="productDetailsDescription">{product?.description}</div>
         </div>
